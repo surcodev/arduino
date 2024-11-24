719e7abd8b2dfc4e7f3e53a3691f676b2c5b639f
surcodev <temporal98x@gmail.com>
2024-11-24 01:13:19 -0500

    Create 03-sensor-dht11-temperatura.ino

---
+++ 03-sensor-dht11-temperatura.ino
@@ -0,0 +1,26 @@
+#include <DHT.h>
+
+#define DHTTYPE DHT11
+#define DHTPIN 9
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup(){
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop(){
+  delay(2000);
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  if(isnan(humedity) || isnan(temperaature)){
+    Serial.print(“Error en el sensor”); 
+  }
+  Serial.print("Temperatura: ");
+  Serial.print(temperature);
+  Serial.print("Humedad: ");
+  Serial.println(humidity);
+  delay(1000);
+}
